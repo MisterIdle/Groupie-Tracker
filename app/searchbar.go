6911728189f3153ae4c6bf10456fdf88e3ffa5ed
Alexy HOUBLOUP <alexyhoublo@gmail.com>
2024-03-25 12:39:18 +0100

    ajout date du premier album dans le search

--- app/searchbar.go
+++ app/searchbar.go
@@ -10,7 +10,12 @@ import (
 )
 
 func (ga *GroupieApp) searchArtists(query string) {
+	ga.showLoadingText(true)
+
 	filteredCards := ga.filterCards(query)
+
+	ga.showLoadingText(false)
+
 	if len(filteredCards) == 0 {
 		noResultsLabel := widget.NewLabel("Aucun résultat trouvé pour la recherche : " + query)
 		ga.content.Objects[1] = noResultsLabel
@@ -24,16 +29,25 @@ func (ga *GroupieApp) searchArtists(query string) {
 	ga.search.SetText("")
 }
 
+func (ga *GroupieApp) showLoadingText(show bool) {
+	if show {
+		ga.content.Objects[1] = widget.NewLabel("Chargement...")
+	} else {
+		ga.content.Objects[1] = container.NewVScroll(container.NewGridWithColumns(3, ga.createArtistCards()...))
+	}
+}
+
 func (ga *GroupieApp) updateSuggestions(query string) {
 	ga.suggestionsBox.Objects = nil
 
 	if query != "" {
 		var filtered []Artist
 
-		// Check if the query is a valid date
 		queryInt, err := strconv.Atoi(query)
 		if err == nil {
 			filtered = ga.filterArtistsByCreationDate(queryInt)
+		} else if strings.Contains(query, "") {
+			filtered = ga.filterArtistByFirstAlbum(query)
 		} else {
 			filtered = ga.filterArtistsAndGroups(query)
 		}
@@ -84,7 +98,21 @@ func (ga *GroupieApp) filterArtistsByCreationDate(date int) []Artist {
 	for _, artist := range ga.artists {
 		yearStr := strconv.Itoa(artist.CreationDate)
 
+		// Si la date est contenue dans l'année de création de l'artiste
 		if strings.Contains(yearStr, strconv.Itoa(date)) {
+			filtered = append(filtered, artist)
+		}
+	}
+
+	return filtered
+}
+
+// "firstAlbum":"14-12-1973"
+func (ga *GroupieApp) filterArtistByFirstAlbum(album string) []Artist {
+	var filtered []Artist
+
+	for _, artist := range ga.artists {
+		if strings.Contains(artist.FirstAlbum, album) {
 			filtered = append(filtered, artist)
 		}
 	}
@@ -103,6 +131,11 @@ func (ga *GroupieApp) filterCards(query string) []fyne.CanvasObject {
 		dateSearch = true
 	}
 
+	// Si la requête est vide, retourner simplement toutes les cartes
+	if query == "" {
+		return ga.createArtistCards()
+	}
+
 	for _, artist := range ga.artists {
 		if strings.Contains(strings.ToLower(artist.Name), queryLower) {
 			filtered = append(filtered, ga.createCard(artist))
@@ -115,6 +148,10 @@ func (ga *GroupieApp) filterCards(query string) []fyne.CanvasObject {
 			}
 		}
 		if dateSearch && artist.CreationDate == queryInt {
+			filtered = append(filtered, ga.createCard(artist))
+		}
+
+		if strings.Contains(artist.FirstAlbum, query) {
 			filtered = append(filtered, ga.createCard(artist))
 		}
 	}
